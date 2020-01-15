28885febfe2475b1e0c0feb2ee6931fb61c9eb3a
stephan <stephan.schutter@gmail.com>
2020-01-15 22:13:30 +0100

    tweaks

--- task.sh
+++ task.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-
+echo "Running $0"
 #wget -O /opt/rtd/scripts/post-install.sh "https://github.com/vonschutter/Blind_Install/raw/master/task.sh" --no-check-certificate 
 #mkdir  -p --mode=0700 /root/.ssh && cat /custom/userkey.pub > /root/.ssh/authorized_keys 
 #[[ -d /home/tangarora ]] && (mkdir --mode=0700 /home/tangarora/.ssh && cat /custom/userkey.pub > /home/tangarora/.ssh/authorized_keys)
