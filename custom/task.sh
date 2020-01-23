1c6213177dea6f6ab0a944b8997ce717ea1587d6
vonschutter <stephan.schutter@gmail.com>
2020-01-23 20:20:25 +0100

    Disabling auto decryption due to bugs.

--- custom/task.sh
+++ custom/task.sh
@@ -167,87 +167,56 @@ OEM_SDDM_LOGIN_OPTION
 
 task_oem_autounlock_disk() {
 	# Setup automatic unlocking of the encrypted system disk (defaul on RTD systems).
-# 1. Back up your initramfs disk
-#
-# Run the following commands in the Dradis console as root:
-# cp /boot/initrd.img-X.Y.Z-N-amd64 /boot/initrd.img-X.Y.Z-N-amd64.safe
-# Make sure to change X.Y.Z-N to match the actual file in your instance. A valid command example:
-# cp /boot/initrd.img-4.9.0-7-amd64 /boot/initrd.img-4.9.0-7-amd64.safe
-#Optionally add a new entry in the boot menu to fall back to the safe initramfs disk:
-# vi /boot/grub/grub.cfg
-# Edit /boot/grub/grub.cfg to add the following:
-#
-### BEGIN /etc/grub.d/10_linux ###
-#...
-# menuentry 'Debian GNU/Linux, with Linux 4.9.0-7-amd64 (crypto safe)' --class debian --class gnu-linux --class gnu --class os {
-#       load_video
-#       insmod gzio
-#       insmod part_msdos
-#       insmod ext2
-#       set root='hd0,msdos1'
-#       search --no-floppy --fs-uuid --set=root 2a5e9b7f-2128-4a50-83b6-d1c285410145
-#       echo    'Loading Linux 4.9.0-7-amd64 ...'
-#       linux   /vmlinuz-4.9.0-7-amd64 root=/dev/mapper/dradispro-root ro  quiet
-#       echo    'Loading initial ramdisk ...'
-#       initrd  /initrd.img-4.9.0-7-amd64.safe
-# }
-# ...
-### END /etc/grub.d/10_linux ###
-# NOTE: Make sure the existing values in that file match the new contents added now:
-# Customization 18
+	# NOTE: This will render the encryption useless since the key to unlock the encrypted
+	# volume will be located on an unencrypted location on the same system as the encrypted volume. 
+	#
+	# The intention behind this is to be able to complete all build activites without manual intervention
+	# of any kind. The intension is to remove the key file after all administrative tasks are complete. 
 
-# 2. Create the key file in the unencrypted /boot partition
+	# 1. Back up your initramfs disk
+	cp  /boot/initrd.img-$(uname -r)  /boot/initrd.img-$(uname -r).bak
 
-dd if=/dev/urandom of=/boot/keyfile bs=1024 count=4
-# 3. Set permissions
+	### BEGIN /etc/grub.d/10_linux ###
+	#...
+	# cat << OEM_CRYPTLOCK_OPTION > /boot/grub/grub.cfg
+	# menuentry 'Debian GNU/Linux, with Linux 4.9.0-7-amd64 (crypto safe)' --class debian --class gnu-linux --class gnu --class os {
+	#       load_video
+	#       insmod gzio
+	#       insmod part_msdos
+	#       insmod ext2
+	#       set root='hd0,msdos1'
+	#       search --no-floppy --fs-uuid --set=root 2a5e9b7f-2128-4a50-83b6-d1c285410145
+	#       echo    'Loading Linux 4.9.0-7-amd64 ...'
+	#       linux   /vmlinuz-4.9.0-7-amd64 root=/dev/mapper/dradispro-root ro  quiet
+	#       echo    'Loading initial ramdisk ...'
+	#       initrd  /initrd.img-4.9.0-7-amd64.safe
+	# }
+	# ...
+	### END /etc/grub.d/10_linux ###
+	# OEM_CRYPTLOCK_OPTION
 
-chmod 0400 /boot/keyfile
+	# 2. Create the key file in the unencrypted /boot partition
+	dd if=/dev/urandom of=/boot/keyfile bs=1024 count=4
 
-# 4. Add the new file as unlock key to the encrypted volume
-echo letmein1234 | cryptsetup -v luksAddKey $(blkid | grep crypto_LUKS|  cut -d : -f 1) /boot/keyfile -
+	# 3. Set permissions
+	chmod 0400 /boot/keyfile
 
-# ls -l /dev/disk/by-uuid/
-# cryptsetup luksChangeKey $(blkid | grep crypto_LUKS|  cut -d : -f 1)
+	# 4. Add the new file as unlock key to the encrypted volume
+	echo letmein1234 | cryptsetup -v luksAddKey $(blkid | grep crypto_LUKS|  cut -d : -f 1) /boot/keyfile -
 
-# 6. Edit /etc/crypttab
-#
-# Edit the contents of file /etc/crypttab (use the UUID of /dev/sda1 from the previous step)
-# vi /etc/crypttab
-# This contents should be:
-# sda5_crypt UUID=9b7200b5-0e0a-447a-93a8-7eb8f1f4a1ee none luks
-# (The UUID may be different)
-#
-# The changes we'll be making:
-#    Replace the 3rd parameter ‐ none ‐ with /dev/disk/by-uuid/<uuid>:/keyfile with the UUID for sda1
-#    Replace the 4th parameter ‐ luks‐ with luks,keyscript=/lib/cryptsetup/scripts/passdev
-#
-# The final result:
-# $(udevadm info $(blkid | grep crypto_LUKS|  cut -d : -f 1) |grep by-uuid | cut -d : -f 2 | head -1)
-chmod 0777 /etc/crypttab
-# sed -i s/oem-replace-me-desktop-selection/$PREFERENCE/g "$SCRIPT_DIR/custom/preseed.cfg"
-cp /etc/crypttab /etc/crypttab.temporary
-sed -i /"$(cat /etc/crypttab | cut -d " " -f 1 )"/d /etc/crypttab.temporary 
-echo $(cat /etc/crypttab | cut -d " " -f 1-2)  /$(udevadm info $(blkid | grep crypto_LUKS|  cut -d : -f 1) |grep by-uuid | cut -d : -f 2 | head -1):/keyfile luks,keyscript=/lib/cryptsetup/scripts/pa$  >> /etc/crypttab.temporary
-mv /etc/crypttab /etc/crypttab.back
-mv /etc/crypttab.temporary /etc/crypttab
+	# 6. Edit /etc/crypttab
+	chmod 0777 /etc/crypttab
+	cp /etc/crypttab /etc/crypttab.temporary
+	sed -i /"$(cat /etc/crypttab | cut -d " " -f 1 )"/d /etc/crypttab.temporary 
+	echo $(cat /etc/crypttab | cut -d " " -f 1-2)  /$(udevadm info $(blkid | grep crypto_LUKS|  cut -d : -f 1) |grep by-uuid | cut -d : -f 2 | head -1):/keyfile luks,keyscript=/lib/cryptsetup/scripts/pa$  >> /etc/crypttab.temporary
+	mv /etc/crypttab /etc/crypttab.back
+	mv /etc/crypttab.temporary /etc/crypttab
 
-#sed -i s/"$oem_old_crypttab_line"/"$oem_new_crypttab_line"/g "/etc/crypttab"
-chmod 0440 /etc/crypttab
+	# Restore permissions to crypttab
+	chmod 0440 /etc/crypttab
 
-
-
-#
-# In this case the UUID for our /dev/sda1 UUID was 2a5e9b7f....
-# If you run into any issues with file permissions, run:
-# chmod 0777 /etc/crypttab
-# After editing, run the following to reset the permissions:
-# chmod 0440 /etc/crypttab
-#
-# 7. Generate a new initramfs disk
-#
-# apt remove 
- mkinitramfs -o /boot/initrd.img-$(uname -r)  $(uname -r)
-
+	# Generate new initramfs
+	mkinitramfs -o /boot/initrd.img-$(uname -r)  $(uname -r)
 
 }
 
@@ -267,4 +236,4 @@ task_setup_ssh_keys		&>> $_LOGFILE
 task_setup_oem_run_once		&>> $_LOGFILE
 task_enable_oem_finish		&>> $_LOGFILE
 task_ensure_oem_auto_login	&>> $_LOGFILE
-task_oem_autounlock_disk	&>> $_LOGFILE
+#task_oem_autounlock_disk	&>> $_LOGFILE
